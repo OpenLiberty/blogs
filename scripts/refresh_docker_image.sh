9a59487e7e7bf779b7d17594391bd45b80f82c30
Steven Zvonek <szvonek@us.ibm.com>
2020-05-13 14:17:34 -0500

    Update refresh_docker_image.sh

--- scripts/refresh_docker_image.sh
+++ scripts/refresh_docker_image.sh
@@ -1,4 +1,4 @@
 docker exec -it website rm -rf /home/jekyll/src/main/content/_posts /home/jekyll/src/main/content/img/blog
-docker cp blogs/publish website:/home/jekyll/src/main/content/_posts
+docker cp blogs/posts website:/home/jekyll/src/main/content/_posts
 docker cp blogs/img/blog website:/home/jekyll/src/main/content/img
 docker cp blogs/blog_tags.json website:/home/jekyll/src/main/content
