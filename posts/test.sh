59845130320787623bfacb31097a6fd11c0bf84c
shamjith <shamjith@shamjiths-MacBook-Pro.local>
2022-09-29 15:34:46 -0400

    added more contents

---
+++ posts/test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+vpc="openliberty-vpc" 
+region="ca-central-1"
+aws ec2 describe-vpc-peering-connections --region $region --filters 'Name=requester-vpc-info.vpc-id,Values='$vpc | grep VpcPeeringConnectionId
+aws ec2 describe-nat-gateways --region $region --filter 'Name=vpc-id,Values='$vpc | grep NatGatewayId
+aws ec2 describe-instances --region $region --filters 'Name=vpc-id,Values='$vpc | grep InstanceId
+aws ec2 describe-vpn-gateways --region $region --filters 'Name=attachment.vpc-id,Values='$vpc | grep VpnGatewayId
+aws ec2 describe-network-interfaces --region $region --filters 'Name=vpc-id,Values='$vpc | grep NetworkInterfaceId
